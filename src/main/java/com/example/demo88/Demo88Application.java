e3fc9c6b271951d627d102dc7e1d7d6ca7b5e981
glzzy123 <2836769325@qq.com>
2023-06-10 10:36:11 +0800

    rewrwe

---
+++ src/main/java/com/example/demo88/Demo88Application.java
@@ -0,0 +1,13 @@
+package com.example.demo88;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo88Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo88Application.class, args);
+	}
+
+}
